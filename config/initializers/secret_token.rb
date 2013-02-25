b2a5f0f8e89c3659ec7c3f92de3ee53555d23378
Maria Fischinger <mmfischinger@gmail.com>
2013-02-25 17:13:06 -0500

    Finish user signup after restoring deleted information

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'a8c164bccde1952e820d15d9fe4fa200edd84d9e0e54010c9cc82947107fb8c60e24ebc47e200f480e9d84daf470827ca244a819172a27ffa2bda531b97b7259'
